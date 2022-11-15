1199835359525c1a90e983d2018a68565b6e088a
Tom <42585270+tjhansult@users.noreply.github.com>
2022-11-15 09:43:07 +0100

    a+sdifh

--- ss/week1/hotel/SimpleBillPrinter.java
+++ ss/week1/hotel/SimpleBillPrinter.java
@@ -8,7 +8,10 @@ public class SimpleBillPrinter {
         String bill = "";
         String description = "Hotel night 1x";
         double value = 85.50;
-        bill = String.format(description+"%25.8f",value);
+        bill = String.format(description + "%25.8f", value);
         System.out.println(bill);
+    }
+    public static void test(){
+        System.out.println("This works");
     }
 }
